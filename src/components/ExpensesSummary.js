ae18cd42099ddb5fee7fb9f86a593240ac8cf14a
Regan <reganjones369@live.co.uk>
2021-02-17 12:30:34 +0000

    syntax changes

--- src/components/ExpensesSummary.js
+++ src/components/ExpensesSummary.js
@@ -1,16 +1,18 @@
-import React from 'react';
-import { connect } from 'react-redux';
-import numeral from 'numeral';
-import selectExpenses from '../selectors/expenses';
-import selectExpensesTotal from '../selectors/expenses-total';
+import React from "react";
+import { connect } from "react-redux";
+import numeral from "numeral";
+import selectExpenses from "../selectors/expenses";
+import selectExpensesTotal from "../selectors/expenses-total";
 
 export const ExpensesSummary = ({ expenseCount, expensesTotal }) => {
-  const expenseWord = expenseCount === 1 ? 'expense' : 'expenses' ;
-  const formattedExpensesTotal = numeral(expensesTotal / 100).format('$0,0.00');
-  
+  const expenseWord = expenseCount === 1 ? "expense" : "expenses";
+  const formattedExpensesTotal = numeral(expensesTotal / 100).format("$0,0.00");
+
   return (
     <div>
-      <h1>Viewing {expenseCount} {expenseWord} totalling {formattedExpensesTotal}</h1>
+      <h1>
+        Viewing {expenseCount} {expenseWord} totalling {formattedExpensesTotal}
+      </h1>
     </div>
   );
 };
@@ -20,7 +22,7 @@ const mapStateToProps = (state) => {
 
   return {
     expenseCount: visibleExpenses.length,
-    expensesTotal: selectExpensesTotal(visibleExpenses)
+    expensesTotal: selectExpensesTotal(visibleExpenses),
   };
 };
 

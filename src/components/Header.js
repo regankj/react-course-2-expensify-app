e146fc65918c0816ce0fccb4c7fcde32b5ac6376
Regan <reganjones369@live.co.uk>
2021-02-22 15:03:53 +0000

    added private routes

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,16 +1,23 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
+import { connect } from "react-redux";
+import { startLogout } from "../actions/auth";
 
-const Header = () => (
+export const Header = ({ startLogout }) => (
   <header>
     <h1>Expensify</h1>
-    <NavLink to="/" exact={true} activeClassName="is-active">
-      Home
+    <NavLink to="/dashboard" exact={true} activeClassName="is-active">
+      Dashboard
     </NavLink>
     <NavLink to="/create" activeClassName="is-active">
       Create
     </NavLink>
+    <button onClick={startLogout}>Log out</button>
   </header>
 );
 
-export default Header;
+const mapDispatchToProps = (dispatch) => ({
+  startLogout: () => dispatch(startLogout()),
+});
+
+export default connect(undefined, mapDispatchToProps)(Header);
--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -17,8 +17,9 @@ var config = {
 firebase.initializeApp(config);
 
 const database = firebase.database();
+const googleAuthProvider = new firebase.auth.GoogleAuthProvider();
 
-export { firebase, database as default };
+export { firebase, googleAuthProvider, database as default };
 
 // database.ref("expenses").on("child_removed", (snapshot) => {
 //   console.log(snapshot.key, snapshot.val());
